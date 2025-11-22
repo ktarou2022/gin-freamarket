4026ae995d003aab7f3511848943c34ea3b4d78d
ktarou2022 <k-taoruzero3@hotmail.co.jp>
2025-11-22 07:19:56 +0000

    get user from token

--- services/auth_service.go
+++ services/auth_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"fmt"
 	"gin-fleamarket/models"
 	"gin-fleamarket/repositories"
 	"os"
@@ -13,6 +14,7 @@ import (
 type IAuthService interface {
 	Siginup(emial string, password string) error
 	Login(emial string, psssword string) (*string, error)
+	GetUserFromToken(tokenString string) (*models.User, error)
 }
 
 type authService struct {
@@ -30,10 +32,10 @@ func (s *authService) Siginup(emial string, password string) error {
 	}
 
 	newUser := models.User{
-		Email: emial,
+		Email:    emial,
 		Password: string(hashedPassword),
 	}
-	return  s.repository.CreateUser(newUser)
+	return s.repository.CreateUser(newUser)
 }
 
 func (s *authService) Login(email string, password string) (*string, error) {
@@ -57,9 +59,9 @@ func (s *authService) Login(email string, password string) (*string, error) {
 
 func CreateToken(userId uint, email string) (*string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub": userId,
+		"sub":   userId,
 		"email": email,
-		"exp": time.Now().Add(time.Hour).Unix(),
+		"exp":   time.Now().Add(time.Hour).Unix(),
 	})
 
 	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
@@ -67,4 +69,29 @@ func CreateToken(userId uint, email string) (*string, error) {
 		return nil, err
 	}
 	return &tokenString, nil
+}
+
+func (s *authService) GetUserFromToken(tokenString string) (*models.User, error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing metohd: %v", t.Header["alg"])
+		}
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var user *models.User
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			return nil, jwt.ErrTokenExpired
+		}
+
+		user, err = s.repository.FindUser(claims["email"].(string))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return user, nil
 }
